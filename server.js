1ac3cd3532e15ee0138516eafc80cf9f84832343
Murfi75 <vlad.balesnyy@gmail.com>
2025-01-15 19:19:20 +0100

    miao

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    console.log(req.url, req.method);
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Тестовый сервер запущен');
+});
